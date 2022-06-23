2bd06f762b163da4b8f33d7d7eeb6c5a1b0da82c
Aneks <63676435+Aneks1@users.noreply.github.com>
2022-06-23 14:12:02 -0500

    Delete config.ts

--- src/Utils/config.ts
+++
@@ -1,9 +0,0 @@
-
-const params = {
-
-    token: "ODMxOTA1ODY3Njg3Mzk1NDAx.YHcCtg.8dM972gZwi9usOk69Nd1YNgP85Y",
-    mongoPath: "mongodb+srv://ItsAmex:COD3-Amex@n3x.rhlgb.mongodb.net/n3x?retryWrites=true&w=majority"
-
-}
-
-export default params
